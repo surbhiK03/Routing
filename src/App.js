a1d5d25c34ba034fa6bdd3dd51dc9bb6a4f3b029
surbhiK03 <kaushiksurbhi233@gmail.com>
2024-05-09 12:49:46 +0530

    routing

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './component/comman/Header';
+import Footer from './component/comman/Footer';
+import MainRoutes from './MainRoutes';
+
+function App() {
+  return (
+    <div className="mainLayout">
+      
+
+      <Header/>
+      <MainRoutes/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
